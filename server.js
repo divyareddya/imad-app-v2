4a5c67499400a14f442b241760dc0eb01f539634
divya reddy <divyareddy423@gmail.com>
2017-02-07 11:49:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,45 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var content - {
+    title: 'article-one | Divya',
+    heading: 'Article one',
+    date: 'sep 5, 2016',
+    content: `
+    <p>this is the content for my first article</p>
+    <p> Fix #1: Run diskpart. If the 0xE0000100 error appears while you're trying to install Windows using the installation DVD, you can use diskpart to clean your partitions first and then install Windows again. Running the clean command of the diskpart utility will remove all data from the partition.</p>
+    `
+    
+};
+
+var htmltemplate - `
+<html>
+    <head>
+        <title>
+          ${title}
+        </title>
+        <meta name-"viewport" content-"width-device-width, initial-scale-1" />
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+    <div class="container">
+        <h1>
+            ${heading}
+        </h1>
+        <div>
+        ${date}
+        </div>
+        <div>
+        ${content}
+        </div>
+    </div>
+    </body>
+</html>
+
+
+`;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
