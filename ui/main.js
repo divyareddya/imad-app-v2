a132c1c39e78f0bd3f31f1fecf9b77d1e09209a4
divya reddy <divyareddy423@gmail.com>
2017-02-21 18:39:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,3 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML = 'new VAlue';
 
-var img=document.getElementById('madi');
-img.onClick = function()
-{
-    img.style.marginLeft = '100px';
-};
