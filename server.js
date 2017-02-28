452de83ac62f3be6264556a8e3ee392a305cab8a
divya reddy <divyareddy423@gmail.com>
2017-02-28 14:02:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,42 +14,7 @@ var config = {
     password: process.env.DBPASSWORD
 };
 
-var articles = { 
-    'article-one': {
-    title: 'article-one | Divya',
-    heading: 'Article one',
-    date: 'sep 5, 2016',
-    content:
-    `<p>this is the content for my first article</p>
-    <p> Fix #1: Run diskpart. If the 0xE0000100 error appears while you're trying to install Windows using the installation DVD, you can use diskpart to clean your partitions first and then install Windows again. Running the clean command of the diskpart utility will remove all data from the partition.</p> `
-    
-},
- 'article-two': {
-    title: 'article-two | Divya',
-    heading: 'Article two',
-    date: 'sep 10, 2016',
-    content:
-    `<p>this is the content for my second article</p>
-    <p>To create a PivotTable:
-Select the table or cells (including column headers) containing the data you want to use. ...
-From the Insert tab, click the PivotTable command. ...
-The Create PivotTable dialog box will appear. ...
-A blank PivotTable and Field List will appear on a new worksheet</p>`
 
-    
-},
- 'article-three':{
-    title: 'article-third | Divya',
-    heading: 'Article third',
-    date: 'sep 15, 2016',
-    content:
-    `<p>this is the content for my third article</p>
-    <p>The selected fields will be added to one of the four areas below the Field List. In our example, the Salesperson field has been added to the Rows area, while the Order Amount has been added to the Values area. Alternatively, you can click, hold, and drag a field to the desired area.</p>`
-    
-    
-    
-}
-};
 var pool = new Pool(config);
 app.get('/test-db',function(req, res)
 {
@@ -105,9 +70,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/:articleName',function(req, res){
+app.get('/article/:articleName',function(req, res){
     var articleName = req.params.articleName;
-  res.send(createTemplate(articles[articleName]));
+    
+    pool.query("SELECT * FROM mytable WHERE console = " + req.params.articleName)
+    var articleData;
+  res.send(createTemplate(articleData));
 });
 
 app.get('/ui/style.css', function (req, res) {
